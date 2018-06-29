ceb487eda2cd1a6f1343a727e80e28161df85db1
shtnkgm <nakagami.shota@gmail.com>
2018-06-30 02:34:31 +0900

    Initial Commit

---
+++ AnimationSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimationSample
+//
+//  Created by 中神 将太 on 2018/06/30.
+//  Copyright © 2018年 Shota Nakagami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
